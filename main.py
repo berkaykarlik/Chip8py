53547d8a6593a296e63a83a1be5cc0c57040f4c2
berkaykarlik <brkykrlk@gmail.com>
2021-12-19 00:32:21 +0300

    decode stage

--- main.py
+++ main.py
@@ -20,13 +20,28 @@ def main():
     while(True):
         # fetch
         curr_instr = mem.fetch()
-        print(curr_instr.hex())
-        # decode
-        # execute
-
         if curr_instr == b'\x00\x00':
-            print("avoiding infinity, goodbye")
             break
+
+        # decode & execute
+        instr_type = int.from_bytes(curr_instr, 'big') & 0xF000
+
+        match instr_type:
+            case 0x0000:
+                print("clear screen")
+            case 0x1000:
+                print("jump")
+            case 0x6000:
+                print("set register")
+            case 0x7000:
+                print("add value to register")
+            case 0xA000:
+                print("set index register")
+            case 0xD000:
+                print("display")
+            case _:
+                print(
+                    f"not implemented: {curr_instr.hex()} type {hex(instr_type)}")
 
 
 if __name__ == '__main__':
