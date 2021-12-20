f2f2b1af899d7762f6f5beeee0877ad1d684dfc7
berkaykarlik <brkykrlk@gmail.com>
2021-12-20 23:41:38 +0300

    add Timers

--- timers.py
+++ timers.py
@@ -1,4 +1,6 @@
-from time import sleep
+import time
+from threading import Timer
+
 FREQ = 60  # hz
 DELAY_LIMIT = 256  # 1 byte
 
@@ -8,16 +10,31 @@ class DelayTimer():
         self.__value = 0
         pass
 
-    def delay(self, delay_value):
-        self.__value = delay_value % DELAY_LIMIT
-        while self.__value > 0:
-            sleep(1/60)
+    def set(self, value: int) -> None:
+        self.__value = value
+        Timer(1/60, self.decrement)
+
+    def get(self) -> None:
+        return self.__value
+
+    def decrement(self) -> None:
+        if self.__value > 0:
             self.__value -= 1
+            Timer(1/60, self.decrement)
 
 
 class SoundTimer():
     def __init__(self) -> None:
         pass
+
+    def set(self, value: int) -> None:
+        self.__value = value
+        Timer(1/60, self.decrement)
+
+    def decrement(self) -> None:
+        if self.__value > 0:
+            self.__value -= 1
+            Timer(1/60, self.decrement)
 
 
 if __name__ == '__main__':
