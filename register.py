f7cfff3377ba9a35deefcc6516b2c52629626685
berkaykarlik <brkykrlk@gmail.com>
2021-12-25 21:07:51 +0300

    reg init null

--- register.py
+++ register.py
@@ -4,7 +4,7 @@ import numpy as np
 class Register():
     def __init__(self) -> None:
         self.__I = np.uint8()
-        self.__V_regs = np.ndarray(16, np.uint8)
+        self.__V_regs = np.zeros(16, np.uint8)
 
     def set_I(self, val: int) -> None:
         if val.bit_length() > 16:
