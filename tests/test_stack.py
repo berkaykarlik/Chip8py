c669b6aa0da096680a1817025d37aefc4b683226
berkaykarlik <brkykrlk@gmail.com>
2022-03-26 22:46:24 +0300

    add stack tests

---
+++ tests/test_stack.py
@@ -0,0 +1,19 @@
+import pytest
+from stack import Stack
+
+
+def test_push_pop():
+    stack = Stack()
+    stack.push(0xFF)
+    assert stack.pop() == 0xFF
+
+def test_stack_limit():
+    stack = Stack()
+    for i in range(16):
+        stack.push(i)
+
+    stack.push(i+1)
+
+    # test pop afterwards for all values pushed
+    for i in range(17):
+        assert stack.pop() == 16 - i
