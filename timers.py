16cfe8e348e756c6984e6bac446815d25ec986c1
berkaykarlik <brkykrlk@gmail.com>
2022-03-26 23:23:51 +0300

    fix:  non-preemptive thread messes timing

--- timers.py
+++ timers.py
@@ -1,4 +1,4 @@
-import time
+from time import time
 from threading import Timer
 
 FREQ = 60  # hz
@@ -8,19 +8,31 @@ DELAY_LIMIT = 256  # 1 byte
 class DelayTimer():
     def __init__(self) -> None:
         self.__value = 0
+        self.__time = None
         pass
 
+
     def set(self, value: int) -> None:
+        if value.bit_length() > DELAY_LIMIT:
+            raise TypeError("1 byte max")
         self.__value = value
-        Timer(1/60, self.decrement).start()
+        self.__time = time()
+
 
     def get(self) -> None:
-        return self.__value
+        """calculate how much time passed since last set and return decremented value"""
+        if self.__time is None:
+            raise Exception("Timer not set")
+        else:
+            time_passed = time() - self.__time
+            self.__time = time()
+            self.__value = self.__value - (time_passed * FREQ)
 
-    def decrement(self) -> None:
-        if self.__value > 0:
-            self.__value -= 1
-            Timer(1/60, self.decrement).start()
+            if self.__value <= 0:
+                self.__time = None
+                self.__value = 0
+
+            return self.__value
 
 
 class SoundTimer():  # TODO:beep somehow
@@ -37,9 +49,3 @@ class SoundTimer():  # TODO:beep somehow
             Timer(1/60, self.decrement).start()
 
 
-if __name__ == '__main__':
-    t = DelayTimer()
-    t.set(60)
-
-    while t.get() != 0:
-        print(t.get())
