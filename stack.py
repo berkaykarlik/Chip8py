8fa06e2da6a43499ce06b4e1e351f194f27e21c2
berkaykarlik <brkykrlk@gmail.com>
2022-03-26 22:46:40 +0300

    raise stack size to 16

--- stack.py
+++ stack.py
@@ -6,7 +6,7 @@ class Stack():
         # plot twist stack was a list all along
         self.__stack = []
         # it seeems back then tere was a limit, so lets pretend we dont have 8gb ram
-        self.__stack_limit = 12
+        self.__stack_limit = 16 #or 12?
 
     def push(self, item: int):
         if len(self.__stack) > self.__stack_limit:
@@ -23,18 +23,3 @@ class Stack():
 
     def __repr__(self) -> str:
         return 'bottom -> ' + ' '.join(list(map(str, self.__stack))) + ' <- top'
-
-
-if __name__ == '__main__':
-    stack = Stack()
-
-    for i in range(15):
-        stack.push(i)
-
-    print(stack)
-
-    while(stack.pop()):
-        print(stack)
-        pass
-
-    print(stack)
