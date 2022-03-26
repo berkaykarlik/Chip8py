313699166a9f0ce3b83486415823029747749848
berkaykarlik <brkykrlk@gmail.com>
2022-03-26 22:47:00 +0300

    add bit length check to set_vx

--- register.py
+++ register.py
@@ -11,13 +11,14 @@ class Register():
             raise TypeError("I register can only be set to 16 bit int")
         self.__I = val
 
+    #TODO: should vf be restricted or sepereated?
     def set_Vx(self, reg_ind: int, val: int) -> None:
         if reg_ind < 0x0 or reg_ind > 0xF:
             raise IndexError(
                 f"we dont have register V{reg_ind}, available index 0x0-0xF")
-        # if val.bit_length() > 8:
-        #     raise TypeError(
-        #         f"V{reg_ind} register can only be set to 8 bit int")
+        if val.bit_length() > 8:
+            raise TypeError(
+                f"V{reg_ind} register can only be set to 8 bit int")
         self.__V_regs[reg_ind] = val
 
     def get_I(self) -> int:
