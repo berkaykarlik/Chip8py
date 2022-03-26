7c88790bf94dd9e2ca2ae18591dc9b531c3a0d82
berkaykarlik <brkykrlk@gmail.com>
2022-03-26 22:46:16 +0300

    add register tests

---
+++ tests/test_register.py
@@ -0,0 +1,30 @@
+import pytest
+from register import Register
+
+
+def test_I():
+    regs = Register()
+    regs.set_I(0xFF)
+    assert regs.get_I() == 0xFF
+
+    #test setting more than 2 bytes
+    with pytest.raises(TypeError) as e_info:
+        regs.set_I(0xFFFFF)
+
+
+def test_Vx():
+    regs = Register()
+    regs.set_Vx(0x0, 0xFF)
+    assert regs.get_Vx(0x0) == 0xFF
+
+    #test setting reg_ind above F
+    with pytest.raises(IndexError) as e_info:
+        regs.set_Vx(0x10, 0xFF)
+
+    #test setting reg_ind below 0
+    with pytest.raises(IndexError) as e_info:
+        regs.set_Vx(-1, 0xFF)
+
+    #test setting more than 1 byte
+    with pytest.raises(TypeError) as e_info:
+        regs.set_Vx(0x0, 0xFFF)
